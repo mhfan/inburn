b9765f443bf1c5416083171ff55d57d4836ed6d6
MeiHui FAN <mhfan@ustc.edu>
2025-02-13 14:48:37 +0800

    refined and bump version of burn to 0.16

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Creation of two tensors, the first with explicit values
     // and the second one with ones, with the same shape as the first
     let tensor_1 = Tensor::<Backend, 2>::from_data([[2., 3.], [4., 5.]], &Default::default());
-    let tensor_2 = Tensor::<Backend, 2>::ones_like(&tensor_1);
+    let tensor_2 = Tensor::ones_like(&tensor_1);
 
     // Print the element-wise addition (done with the WGPU backend) of the two tensors.
     println!("{}", tensor_1 + tensor_2);
